8f410f4c9415badfff3b2475e0dc0ca8b22f7b71
Temple Lin <78478355+TempleLin@users.noreply.github.com>
2022-10-29 06:33:25 +0800

    Chunk part 1.

--- pt02_build_quad_block_chunk/Assets/Chunk.cs
+++ pt02_build_quad_block_chunk/Assets/Chunk.cs
@@ -1,43 +1,52 @@
-using System;
 using UnityEngine;
 
 /**
  * A chunk represents the largest mesh that you want to have within your world environment.
  * You can't make an absolutely massive world and expect everything to be in the one mesh.
- *
+ * 
  * A chunk is made up of blocks.
  */
 public class Chunk : MonoBehaviour {
+    /**
+     * Material containing Minecraft texture.
+     */
+    [SerializeField] private Material atlas;
+    private readonly int depth = 2; // Z coordinate
+    private readonly int height = 2; // Y coordinate
+
+    /**
+     * A chunk should have width, height, and depth.
+     */
+    private readonly int width = 2; // X coordinate
     /**
      * Take a look at Quad.cs to understand these fields.
      */
     private MeshFilter _meshFilter;
     private MeshRenderer _meshRenderer;
+
     /**
-     * Material containing Minecraft texture.
+     * Use multi-dimensional array to store the blocks into a chunk.
+     * 
+     * This can let game know the position of each block (x,y,z), position of a chunk(chunkSize, chunkSize, chunkSize),
+     * and positioning the chunks next to each other and not overlap the blocks.
      */
-    [SerializeField] private Material atlas;
+    private Mesh[,,] blocks;
+
     private void Start() {
         _meshFilter = gameObject.AddComponent<MeshFilter>();
         _meshRenderer = gameObject.AddComponent<MeshRenderer>();
-        
-        QuadBuilder quadBuilder = new QuadBuilder();
-        /*
-         * Create all quads of a mesh.
-         */
-        Mesh[] quadsMeshes = {
-            quadBuilder.build(MeshUtils.BlockSide.BOTTOM, new Vector3(0, 0, 0)),
-            quadBuilder.build(MeshUtils.BlockSide.TOP, new Vector3(0, 0, 0)),
-            quadBuilder.build(MeshUtils.BlockSide.LEFT, new Vector3(0, 0, 0)),
-            quadBuilder.build(MeshUtils.BlockSide.RIGHT, new Vector3(0, 0, 0)),
-            quadBuilder.build(MeshUtils.BlockSide.FRONT, new Vector3(0, 0, 0)),
-            quadBuilder.build(MeshUtils.BlockSide.BACK, new Vector3(0, 0, 0)),
-        };
+        _meshRenderer.material = atlas;
 
-        /*
-         * Merge all the quad meshes into single mesh through this custom utility method.
-         */
-        _meshFilter.mesh = MeshUtils.mergeMeshes(quadsMeshes);
-        _meshFilter.mesh.name = "Cube_0_0_0";
+        BlockBuilder blockBuilder = new BlockBuilder();
+        
+        blocks = new Mesh[width, height, depth];
+
+        for (int z = 0; z < depth; z++) {
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    blocks[x, y, z] = blockBuilder.build(new Vector3(x, y, z), MeshUtils.BlockType.DIRT, MeshUtils.BlockType.DIRT);
+                }
+            }
+        }
     }
 }
