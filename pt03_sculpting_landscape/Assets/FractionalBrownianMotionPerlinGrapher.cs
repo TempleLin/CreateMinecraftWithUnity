19eb3e2975a6cf7b0f313548a122711788a5edd3
Temple Lin <78478355+TempleLin@users.noreply.github.com>
2022-11-01 23:26:19 +0800

    FBM.

--- pt03_sculpting_landscape/Assets/FractionalBrownianMotionPerlinGrapher.cs
+++ pt03_sculpting_landscape/Assets/FractionalBrownianMotionPerlinGrapher.cs
@@ -1,13 +1,14 @@
-using System;
 using UnityEngine;
 
 /// <summary>
-///     Used for drawing on object, for visualizing noise graph.
-/// Sometimes this component's start might not run in edit mode. Can force it to run by running the game.
+/// Different from PerlinGrapher component, this component not just draws Perlin noise wave lines,
+/// it also implements Fractional Brownian Motion.
+///
+/// Article explaining Fractional Brownian Motion: https://iquilezles.org/articles/fbm/
 /// </summary>
-[ExecuteInEditMode] //This makes it runnable without running game.
+[ExecuteInEditMode]
 [RequireComponent(typeof(LineRenderer))]
-public class PerlinGrapher : MonoBehaviour {
+public class FractionalBrownianMotionPerlinGrapher : MonoBehaviour {
     [SerializeField] private LineRenderer lr;
 
     /// <summary>
@@ -21,6 +22,11 @@ public class PerlinGrapher : MonoBehaviour {
     /// If given a very small value to this value (~0.14f), the wave turns into smoother curves. 
     /// </summary>
     [SerializeField] private float scale = 0.5f;
+
+    [SerializeField] private float octaves = 1;
+
+    [SerializeField] private float heightOffset = 0;
+    
     private void Start() {
         lr = GetComponent<LineRenderer>();
         lr.positionCount = 100;
@@ -48,9 +54,21 @@ public class PerlinGrapher : MonoBehaviour {
         int z = 11;
         Vector3[] positions = new Vector3[lr.positionCount];
         for (int x = 0; x < positions.Length; x++) {
-            float y = Mathf.PerlinNoise(x * scale, z * scale) * heightScale; // Find Perlin noise height value determined by x and z locations.  
+            // float y = Mathf.PerlinNoise(x * scale, z * scale) * heightScale; // Find Perlin noise height value determined by x and z locations.
+            float y = fBM(x, z) + heightOffset;
             positions[x] = new Vector3(x, y, z);
         }
         lr.SetPositions(positions);
+    }
+
+    private float fBM(float x, float z) {
+        float total = 0;
+        float frequency = 1;
+        for (int i = 0; i < octaves; i++) {
+            total += Mathf.PerlinNoise(x * scale * frequency, z * scale * frequency) * heightScale;
+            frequency *= 2;
+        }
+
+        return total;
     }
 }
